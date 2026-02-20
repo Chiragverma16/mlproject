56a8626b55f4e8a8a2a871a679600d7dff0df41e
Chirag verma <chiragverma1602@gmail.com>
2026-02-20 21:43:11 +0530

    Fix model path for deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,23 +3,27 @@ import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 import pickle 
+import os
 
 class PredictPipeline:
     def __init__(self):
         pass
 
     def predict(self, features):
-        try: 
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
-            model = load_object(file_path = model_path)
-            preprocessor = load_object(file_path = preprocessor_path)
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
+
             return preds
-    
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
 
 class CustomData:
@@ -55,5 +59,6 @@ class CustomData:
             return pd.DataFrame(custom_data_input_dict)
         
         except Exception as e:
-            raise CustomException(e, sys)
+            print(e)
+        raise e
         
